e14aa965e37921779f17b4a7120d9ac13b24d630
snopeks <creativesnopeks@gmail.com>
2018-04-02 13:21:20 -0700

    added orbit controls and some styling to world

--- js/app.js
+++ js/app.js
@@ -1,28 +1,48 @@
-var scene = new THREE.Scene();
-			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+window.onload = function(){
 
 			var renderer = new THREE.WebGLRenderer();
 			renderer.setSize( window.innerWidth, window.innerHeight );
 			document.body.appendChild( renderer.domElement );
 
-			var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			var cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
+			var camera = new THREE.PerspectiveCamera( 60, window.innerWidth/window.innerHeight, 1, 20000 );
+			camera.position.set( 0, 0, 20 );
+			camera.lookAt( new THREE.Vector3( 0, 0, 0 ) );
 
-			camera.position.z = 5;
+			var controls = new THREE.OrbitControls( camera, renderer.domElement );
+			// controls.movementSpeed = 1000;
+			// controls.lookSpeed = 0.125;
+			// controls.lookVertical = true;
+			// controls.update();
+			var scene = new THREE.Scene();
+			scene.background = new THREE.Color( 0xbfd1e5 );
+			var geometry2 = new THREE.Geometry();
+			var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+			geometry2.vertices.push(new THREE.Vector3( -10, 0, 0) );
+			geometry2.vertices.push(new THREE.Vector3( 0, 10, 0) );
+			geometry2.vertices.push(new THREE.Vector3( 10, 0, 0) );
+			// console.log(geometry2.vertices)
+
+			var cubeMaterial = new THREE.MeshBasicMaterial( { color: 0x9932CC } );
+			var lineMaterial = new THREE.LineBasicMaterial( { color: 0xff0000 } );
+			var line = new THREE.Line( geometry2, lineMaterial );
+
+
+			var cube = new THREE.Mesh( geometry, cubeMaterial );
+			scene.add( cube );
+			scene.add( line );
+			renderer.render( scene, camera );
+			// camera.position.z = 5;
 
 			var animate = function () {
 				requestAnimationFrame( animate );
 
-				cube.rotation.x += 0.1;
-				cube.rotation.y += 0.1;
+				cube.rotation.x += 0.09;
+				cube.rotation.y += 0.08;
 
 				renderer.render(scene, camera);
 			};
 
-window.onload = function(){
+
   console.log("js is running")
   animate();
-  
 }
